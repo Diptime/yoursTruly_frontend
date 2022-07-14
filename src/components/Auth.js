2dcf2444386084444d0f1bb273bfe123f6a06504
Dipti <diptiimee@gmail.com>
2022-07-14 07:58:17 +0530

    ser-add-update

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -22,7 +22,7 @@ const Auth = () => {
   };
   const sendRequest = async (type = "login") => {
     const res = await axios
-      .post(`https://yourstruly-backend.herokuapp.com/user/${type}`, {
+      .post(`https://yourstruly-backend.herokuapp.com/api/user/${type}`, {
         name: inputs.name,
         email: inputs.email,
         password: inputs.password,
